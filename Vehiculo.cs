8875e32ff77ee58d4767f578af8b77cc21137bc6
luiking85 <luis.avila@pichincha.com.co>
2021-02-01 15:25:24 -0500

    Definicion base prototipo vehiculo

---
+++ Vehiculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+public abstract class Vehiculo
+{
+
+
+    public Vehiculo(List<T> generales, List<T>especificas)
+	{
+
+	}
+
+
+}
+
+public class caracteristicasGenerales
+{
+    public int llantas { get; set; }
+    public int puertas { get; set; }
+    public string placa { get; set; }
+    public int idchasis { get; set; }
+}
+
+public class caracteristicasEspecificas
+{
+    public float cilindraje { get; set; }
+    public int valvulas { get; set; }
+    public float relacioncompresion { get; set; }
+    public tipocombustible combustible { get; set; }
+}
+
+public enum tipocombustible
+{ 
+    "Gas",
+    "Diesel",
+    "Gasolina",
+    "Eletrica"
+}
